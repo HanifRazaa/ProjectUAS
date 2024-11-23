8c52522c21383c55e0b4e98058457c7e3d294dbb
HanifRazaa <hanif.raza@gmail.com>
2024-11-24 06:52:46 +0700

    added veiwdest function

--- test1.cpp
+++ test1.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class user{
+    public:
+    void viewDesti(){
+        cout << "Where We Fly" << endl;
+        cout << "-----------------------------------" << endl;
+        cout << "1. Yogyakarta, Indonesia" << endl;
+        cout << "2. Singapore" << endl;
+        cout << "3. Rome, Italy" << endl;
+        cout << "4. Osaka, Japan" << endl;
+    }
+};
