1b1f5f6d956cd99df80fc77c5f6e3a1decb1864f
HanifRazaa <hanif.raza@gmail.com>
2024-11-25 15:46:59 +0700

    added another class and function

--- test1.cpp
+++ test1.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class user{
     public:
     void viewDesti(){
+        system("cls");
         cout << "Where We Fly" << endl;
         cout << "-----------------------------------" << endl;
         cout << "Yogyakarta, Indonesia" << endl;
@@ -12,31 +14,57 @@ class user{
         cout << "Osaka, Japan" << endl;
     }
     void viewFlights(){
+        system("cls");
         cout << "Flights Available" << endl;
         cout << "-----------------------------------" << endl;
-        cout << "Jakarta (CGK) - Yogyakarta (YIA)" << endl;
-        cout << "Jakarta (CGK) - Singapore (SIN)" << endl;
-        cout << "Jakarta (CGK) - Rome (FCO)" << endl;
-        cout << "Jakarta (CGK) - Osaka (KIX)" << endl;
+        cout << "Jakarta - Yogyakarta " << endl;
+        cout << "Jakarta - Singapore " << endl;
+        cout << "Jakarta - Rome " << endl;
+        cout << "Jakarta - Osaka " << endl;
     }
 
-    void viewMemberstatus(){
-        cout << "Your Frequent Flyer tier is : " << memberStatus << endl;
+    string viewmemberBenefit(){
+        system("cls");
+        cout << "Our Frequent Flyer Benefit" << endl;
         cout << "-----------------------------------" << endl;
-        cout << "Flyer Points : " << flyerPoints << endl;
+        cout << "For Blue tier, you will get" << endl;
+        cout << "Free WiFi on your flights" << endl;
+        cout << "Save 15% for your seat selection" << endl;
         cout << "-----------------------------------" << endl;
-        cout << "In the next flight, you can : " << memberBenefit << endl;
+        cout << "For Maroon tier, you will get" << endl;
+        cout << "all benefits of Blue, plus :" << endl;
+        cout << "Free on-board snacks on all your flights" << endl;
+        cout << "Extra 10 kg baggage allowance" << endl;
+        cout << "-----------------------------------" << endl;
+        cout << "For Gold tier, you will get" << endl;
+        cout << "all benefits of Maroon, plus :" << endl;
+        cout << "Priority check-in " << endl;
+        cout << "Priority boarding" << endl;
+        cout << "Access to our exclusive lounge" << endl;
+        cout << "Save 25% for your seat selection" << endl;
+        cout << "Discount on redemption fee" << endl;
+        cout << "-----------------------------------" << endl;
+        cout << "For Platinum tier, you will get" << endl;
+        cout << "all benefits of Gold, plus :" << endl;
+        cout << "Priority check-in " << endl;
+        cout << "Priority boarding" << endl;
+        cout << "all benefits of Gold, plus :" << endl;
+        cout << "Free seat selection" << endl;
+        cout << "Extra 25 kg baggage allowance" << endl;
+        cout << "Our special service to assist you in airport " << endl;
+        cout << "No redemption fee" << endl;
     }
 };
 
 int flyerPoints(){
     int flyerPoints = 0;
+    return flyerPoints;
 };
 string memberStatus(){
     string memberTier;
     int flyerpoints;
     if (flyerpoints<150){
-        memberTier = "White"; 
+        memberTier = "Blue"; 
     }
     else if (flyerpoints<250){
         memberTier = "Maroon";
@@ -45,7 +73,7 @@ string memberStatus(){
         memberTier = "Gold";
     }
     else if (flyerpoints>500){
-        memberTier = "Black";
+        memberTier = "Platinum";
     }
     return memberTier;
 }
@@ -53,42 +81,132 @@ string memberStatus(){
 string memberBenefit(){
     string benefit;
     string memberTier;
-    if (memberTier == "White"){
-        cout << "" << endl;
+    if (memberTier == "Blue"){
+        cout << "Free WiFi on your flights" << endl;
+        cout << "Save 15% for your seat selection" << endl;
     }
     else if (memberTier == "Maroon"){
-        cout << "" << endl;
+        cout << "all benefits of Blue, plus :" << endl;
+        cout << "Free on-board snacks on all your flights" << endl;
+        cout << "Extra 10 kg baggage allowance" << endl;
     }
     else if (memberTier == "Gold"){
-        cout << "" << endl;
+        cout << "all benefits of Maroon, plus :" << endl;
+        cout << "Priority check-in " << endl;
+        cout << "Priority boarding" << endl;
+        cout << "Access to our exclusive lounge" << endl;
+        cout << "Save 25% for your seat selection" << endl;
+        cout << "Discount on redemption fee" << endl;
     }
-    else if (memberTier == "Black"){
-        cout << "" << endl;
+    else if (memberTier == "Platinum"){
+        cout << "all benefits of Gold, plus :" << endl;
+        cout << "Free seat selection" << endl;
+        cout << "Extra 25 kg baggage allowance" << endl;
+        cout << "Our special service to assist you in airport " << endl;
+        cout << "No redemption fee" << endl;
     }
     return benefit;
-}
-int main(){
-    int choice = UserInput();
-    switch (choice) {
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
     }
-    return 0;
+
+class Admin : public user{
+    private:
+    string name;
+    string id;
+    string password;
+    public:
+    void adminLogin(){
+        system("cls");
+        cout << "Enter your ID and password" << endl;
+    }
+    void createFlyer(){
+
+    }
 };
 
-int UserInput(){
+class Flyer{
+    public:
+    void flyerLogin(){
+        system("cls");
+        cout << "Please enter your flyer ID and password" << endl;
+    }
+    void bookFlight(){
+        int choice;
+        int points = flyerPoints();
+        system("cls");
+        cout << "Choose your Destination" << endl;
+        cout << "1. CGK - YIA" << endl;
+        cout << "2. CGK - SIN" << endl;
+        cout << "3. CGK - FCO" << endl;
+        cout << "4. CGK - KIX" << endl;
+        cin >> choice;
+        if (choice == 1){
+           cout << "Your flight is booked" << endl;
+           points += 60;
+        }
+        else if (choice == 2){
+            cout << "Your flight is booked" << endl;
+            points += 80;
+        }
+        else if (choice == 3){
+            cout << "Your flight is booked" << endl;
+            points += 200;
+        }
+        else if (choice == 4){
+            cout << "Your flight is booked" << endl;
+            points += 150;
+        }
+        
+    }
+    
+    void viewMemberstatus(){
+        string benefit = memberBenefit();
+        int points = flyerPoints();
+        string tier = memberStatus();
+
+        system("cls");
+        cout << "Your Frequent Flyer tier is : " << tier << endl;
+        cout << "-----------------------------------" << endl;
+        cout << "Flyer Points : " << points << endl;
+        cout << "In the next flight, you can : " << benefit << endl;
+    }
+};
+int main(){
+    user u;
+    Admin n;
+    Flyer f;
     int choice;
+    system("cls");
     cout << "Welcome to Fly-Air" << endl;
     cout << "1. View Destination" << endl;
     cout << "2. View Flights" << endl;
-    cout << "3. View Member Status" << endl;
-    cout << "4. Book a Flight" << endl;
-    cout << "5. Admin Login" << endl;
-    cout << "6. Exit" << endl;
+    cout << "3. View Frequent Flyer Benefits" << endl;
+    cout << "4. Frequent Flyer" << endl;
+    cout << "5. Book a Flight" << endl;
+    cout << "6. Admin Login" << endl;
+    cout << "7. Exit" << endl;
+    cout << "Please enter your choice : ";
     cin >> choice;
+    if (choice == 1){
+        u.viewDesti();
+    }
+    else if (choice == 2){
+        u.viewFlights();
+    }
+    else if (choice == 3){
+        u.viewmemberBenefit();
+    }
+    else if (choice == 4){
+        f.flyerLogin();
+    }
+    else if (choice == 5){
+        f.bookFlight();
+    }
+    else if (choice == 6){
+        n.adminLogin();
+    }
+    else {
+        system("cls");
+        cout << "Thank you for choosing our airline" << endl;
+    }
     return choice;
 };
